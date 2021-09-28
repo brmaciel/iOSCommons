5077c38292d115612cc7afab2bdfd5d040fa3192
brmaciel <44143089+brmaciel@users.noreply.github.com>
2021-09-27 21:50:20 -0300

    Move DocumentType to a separate file

---
+++ Sources/iOSCommons/Documents/DocumentType.swift
@@ -0,0 +1,14 @@
+//
+//  DocumentType.swift
+//  
+//
+//  Created by Bruno Maciel on 9/27/21.
+//
+
+import Foundation
+
+public enum DocumentType {
+    case cpf
+    case cnpj
+    case hybridCPFandCNPJ
+}
